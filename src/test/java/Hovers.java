39eec888ce6b3d258a8f0dfc9761d2670c37091a
Yuschkewicz <13and.jei@gmail.com>
2022-03-10 11:52:28 +0300

    Initial commit

---
+++ src/test/java/Hovers.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Hovers extends BaseTest{
+
+    @Test
+    public void hovers(){
+                driver.get("http://the-internet.herokuapp.com/hovers");
+                //  пока в разработке
+
+
+
+
+
+    }
+
+}
---
+++ src/test/java/Inputs.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+
+
+public class Inputs extends  BaseTest {
+
+    @Test
+    public void inputs() {
+
+        driver.get("http://the-internet.herokuapp.com/inputs");
+        driver.findElement(By.tagName("input")).sendKeys("5");
+        driver.findElement(By.tagName("input")).sendKeys(Keys.ARROW_UP);
+        String numb= driver.findElement(By.tagName("input")).getText();
+        assertEquals(numb,6);
+        driver.findElement(By.tagName("input")).sendKeys("r");
+        String value= driver.findElement(By.tagName("input")).getText();
+        assertEquals(value,null);
+
+
+    }
+}
