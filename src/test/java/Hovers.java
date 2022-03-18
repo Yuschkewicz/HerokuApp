9b14989886cc10fb5607e5242ed6131f01f24c55
Yuschkewicz <13and.jei@gmail.com>
2022-03-18 21:18:37 +0300

    0.0.8

--- src/test/java/Hovers.java
+++ src/test/java/Hovers.java
@@ -1,12 +1,10 @@
-import org.testng.annotations.Test;
+
 
 
 public class Hovers extends BaseTest{
 
-    @Test
-    public void hovers(){
-                driver.get("http://the-internet.herokuapp.com/hovers");
                 //  пока в разработке
+    //
 
 
 
@@ -14,4 +12,4 @@ public class Hovers extends BaseTest{
 
     }
 
-}
+
