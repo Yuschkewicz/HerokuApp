701086f6ae0eaf59e69999f125c259f2e16e1efe
Yuschkewicz <13and.jei@gmail.com>
2022-04-13 23:50:33 +0300

    2

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -3,11 +3,15 @@ package Tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Optional;
 
 
 public class BaseTest {
@@ -17,16 +21,39 @@ public class BaseTest {
 
 
     @BeforeMethod
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void setup(@Optional("chrome") String browser, ITestContext testContext) {
+        if (System.getProperty("browser", "chrome").equalsIgnoreCase("chrome")) {
+            if (browser.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                if (System.getProperty("headless", "true").equals("true"))
+                    options.addArguments("--headless");
+                driver = new ChromeDriver(options);
+            } else if (browser.equalsIgnoreCase("EDGE")) {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                driver.manage().window().maximize();
+            } else if (browser.equalsIgnoreCase("Opera")) {
+                WebDriverManager.operadriver().setup();
+                driver = new OperaDriver();
+                driver.manage().window().maximize();
+            } else if (browser.equalsIgnoreCase("FireFox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+            } else if (System.getProperty("browser", "chrome").equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+        }
     }
 
 
     @AfterMethod(alwaysRun = true)
-    public void close(){
-        driver.quit();
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+
+        }
     }
 }
